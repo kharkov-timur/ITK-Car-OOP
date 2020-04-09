cb1e9dd7ab8e22fd9b911d55ada2a0e1cadc2af9
kharkov-timur <8cheri8@gmail.com>
2020-04-09 10:18:55 +0300

    ITK-Car-Переход на ООП

--- js/car.js
+++ js/car.js
@@ -1,22 +1,62 @@
+'use strict';
+
 function carStartListener() {
   var randomNumber = Math.random();
 
   if (randomNumber > 0.5) {
-    alert('Car have started');
-    startButton.classList.add('hide');
-
-    function engineCrashed() {
-      alert('Engine crashed');
-      startButton.classList.remove('hide');
-    }
-    window.setTimeout(engineCrashed, 5000);
-    alert('We wait crash');
-
+    carStarted();
   } else {
-    alert('Something wrong');
+    carCantBeStarted();
   }
 }
 
-var startButton = document.querySelector('#start-csr');
+function drawStatus(status) {
+  statusLabel.innerHTML = status;
+}
 
+function devLog(message) {
+  //console.log(message);
+}
+
+function carCantBeStarted() {
+  devLog('Something wrong');
+  drawStatus('Car can\'t be started. Try again!');
+}
+
+function carStarted() {
+  devLog('Car have started');
+  drawStatus('Car have started');
+  gearBoxStarted();
+  planedCrashStarted();
+  devLog('We wait crash');
+}
+
+function gearBoxStarted() {
+  var gearBoxValue = '1';
+  gearBoxValueLabel.innerHTML = gearBoxValue;
+
+  function incrementGearBoxValue() {
+    if (gearBoxValue < 5) {
+      gearBoxValue++;
+      gearBoxValueLabel.innerHTML = gearBoxValue;
+    }
+  }
+  gearBoxInterval = window.setInterval(incrementGearBoxValue, 1000);
+}
+
+function planedCrashStarted() {
+  function engineCrashed() {
+    devLog('Engine crashed');
+    drawStatus('Engine have crashed. Car stopped');
+    startButton.classList.remove('hide');
+    gearBoxValueLabel.innerHTML = 'N';
+    window.clearInterval(gearBoxInterval);
+  }
+  window.setTimeout(engineCrashed, 3000);
+}
+
+var gearBoxInterval;
+var startButton = document.querySelector('#start-car');
+var statusLabel = document.querySelector('#status');
+var gearBoxValueLabel = document.querySelector('#gear-box-value');
 startButton.addEventListener('click', carStartListener);
